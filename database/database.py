eee9e1d488f7240e85d734d95fc50344724ac66e
Prajwal335 <dushanthgv910855@gmail.com>
2022-04-03 22:30:43 +0530

    Corrected Indentation and Adding Instruction as Comments

--- database/database.py
+++ database/database.py
@@ -6,17 +6,18 @@ import time
 import os
 
 time.sleep(30)
-# read rabbitmq connection url from environment variable
+
+# Read rabbitmq connection url from environment variable
 amqp_url = os.environ['AMQP_URL']
 url_params = pika.URLParameters(amqp_url)
 
-# connect to rabbitmq
+# Connect to rabbitmq
 connection = pika.BlockingConnection(url_params)
 chan = connection.channel()
 
 chan.queue_declare(queue='ride_database', durable=True)
 
-# create a local database
+# Create a local database
 db = TinyDB('./db.json')
 
 def receive_msg(ch, method, properties, body):
@@ -25,13 +26,12 @@ def receive_msg(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-# to make sure the consumer receives only one message at a time
-# next message is received only after acking the previous one
+# To make sure the consumer receives only one message at a time
+# Next message is received only after acking the previous one
 chan.basic_qos(prefetch_count=1)
 
-# define the queue consumption
-chan.basic_consume(queue='ride_database',
-                   on_message_callback=receive_msg)
+# Define the queue consumption
+chan.basic_consume(queue='ride_database', on_message_callback=receive_msg)
 
 chan.start_consuming()
 
